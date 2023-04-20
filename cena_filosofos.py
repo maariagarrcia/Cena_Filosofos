c7c513c8fbcb0a75538900f99c27b45a5535eb07
maariagarrcia <mrrmn@myuax.com>
2023-04-20 12:28:38 +0200

    estructura

---
+++ cena_filosofos.py
@@ -0,0 +1,17 @@
+from colorama import Fore
+import threading
+import time
+import random
+
+class Tenedor():
+    pass
+
+class Filosofo(threading.Thread):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    
