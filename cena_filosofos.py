fdd086936cb96cb83d725eb180b6c723fb24d00d
maariagarrcia <mrrmn@myuax.com>
2023-04-20 12:31:26 +0200

    estructura principal

--- cena_filosofos.py
+++ cena_filosofos.py
@@ -4,14 +4,30 @@ import time
 import random
 
 class Tenedor():
-    pass
+    def __init__():
+        pass
+
+    def usar():
+        pass
+
+    def dejar_de_usar():
+        pass
 
 class Filosofo(threading.Thread):
-    pass
+    def __init__():
+        pass
+
+    def comer():
+        pass
+
+    def run():
+        pass
 
 def main():
-    pass
+    # crer los 5 tenedores
+    # crear los 5 filosofos
+    # Arrancar los 5 filosoos para que empiecen a comer
+    pass 
 
 if __name__ == '__main__':
     main()
-    
